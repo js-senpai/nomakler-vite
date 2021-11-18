9e7d126f1f89e3ce584351d52fff117c5c1b7997
Andrew <fincheran@gmail.com>
2021-11-18 18:53:35 +0200

    add optimization modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 import { UserConfig } from "vite";
-
+import viteCompression from 'vite-plugin-compression';
+import viteImagemin from 'vite-plugin-imagemin';
 const config: UserConfig = {
-  plugins: [react(), ssr()],
+  plugins: [react(), ssr(),viteCompression(),viteImagemin({
+    gifsicle: {
+      optimizationLevel: 5,
+      interlaced: false,
+    },
+    optipng: {
+      optimizationLevel: 5,
+    },
+    mozjpeg: {
+      quality: 20,
+    },
+    pngquant: {
+      quality: [0.8, 0.9],
+      speed: 4,
+    },
+    webp: {
+      quality: 50,
+    },
+    svgo: {
+      plugins: [
+        {
+          name: 'removeViewBox',
+        },
+        {
+          name: 'removeEmptyAttrs',
+          active: false,
+        },
+      ],
+    },
+  }),],
 };
 
 export default config;
